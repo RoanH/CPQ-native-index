7739cfb836db99469f2df4b4aa14ee547b3a916f
Roan Hofland <roan.hofland@hotmail.com>
2023-02-27 15:33:46 +0900

    Initial commit

---
+++ CPQ-native Index/src/dev/roanh/cpqindex/Main.java
@@ -0,0 +1,40 @@
+package dev.roanh.cpqindex;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main{
+
+	
+	
+	
+	public static void main(String[] args){
+		//initialise native bindings
+		try{
+			loadNatives();
+		}catch(IOException | UnsatisfiedLinkError e){
+			e.printStackTrace();
+			return;
+		}
+		
+		
+		
+		
+		//TODO
+	}
+	
+	
+	/**
+	 * Loads the compiled JNI libraries required for nauty.
+	 * @throws IOException When an IOException occurs.
+	 * @throws UnsatisfiedLinkError When loading a native library fails.
+	 */
+	private static final void loadNatives() throws IOException, UnsatisfiedLinkError{
+		for(Path lib : Files.newDirectoryStream(Paths.get("lib"), Files::isRegularFile)){
+			System.out.println("Loading native library: " + lib.getFileName());
+			System.load(lib.toAbsolutePath().toString());
+		}
+	}
+}
