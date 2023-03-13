529c875fb0c3fe0a58d3a2b47330176a88f9ab5e
Roan Hofland <roan.hofland@hotmail.com>
2023-03-13 19:03:23 +0900

    Brief start on equivalence classes

---
+++ CPQ-native Index/src/dev/roanh/cpqindex/EquivalenceClass.java
@@ -0,0 +1,31 @@
+package dev.roanh.cpqindex;
+
+import dev.roanh.gmark.conjunct.cpq.CPQ;
+import dev.roanh.gmark.util.UniqueGraph;
+
+public class EquivalenceClass{
+
+	
+	
+	
+	
+	
+	//partition according to k-path-bisimulation
+	public static <V, E extends Comparable<E>> void partition(UniqueGraph<V, E> g, int k){
+		
+		
+		//CPQ.parse(query)
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//private static final class PathMap<V>
+}
