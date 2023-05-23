824eaa54af9ebcdfde7e21103cb900cf9db6868d
Roan Hofland <roan@roanh.dev>
2023-05-23 20:23:04 +0900

    CLI implementation

--- CPQ-native Index/src/dev/roanh/cpqindex/Main.java
+++ CPQ-native Index/src/dev/roanh/cpqindex/Main.java
@@ -1,12 +1,21 @@
 package dev.roanh.cpqindex;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Duration;
-import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import dev.roanh.gmark.conjunct.cpq.CPQ;
 import dev.roanh.gmark.conjunct.cpq.QueryGraphCPQ;
@@ -16,6 +25,10 @@ import dev.roanh.gmark.util.GraphPanel;
 import dev.roanh.gmark.util.UniqueGraph;
 
 public class Main{
+	/**
+	 * The command line options.
+	 */
+	public static final Options options;
 
 	public static void main(String[] args){
 		//initialise native bindings
@@ -26,16 +39,68 @@ public class Main{
 			return;
 		}
 		
-		//TODO
-		
+		CommandLineParser parser = new DefaultParser();
 		try{
-//			Thread.sleep(10000);
-			System.out.println("START");
-			Instant start = Instant.now();//advogato, robots
-			Index index = new Index(IndexUtil.readGraph(Paths.get("C:\\Users\\roanh\\Documents\\2 Thesis\\Datasets\\robots.edge")), 2, false, true, 7, Integer.MAX_VALUE, ProgressListener.LOG);//cores, labels
-			index.write(Files.newOutputStream(Paths.get("test.bin")), false);
-			System.out.println("done: " + Duration.between(start, Instant.now()).toString());
-		}catch(Exception e){
+			CommandLine cli = parser.parse(options, args);
+			if(!cli.hasOption('h')){
+				handleInput(cli);
+				return;
+			}
+		}catch(ParseException e){
+			System.out.println(e.getMessage());
+		}
+		
+		HelpFormatter help = new HelpFormatter();
+		help.setWidth(120);
+		help.printHelp("index", options, true);
+		
+//		
+//		
+//		//TODO
+//		
+//		try{
+////			Thread.sleep(10000);
+//			System.out.println("START");
+//			Instant start = Instant.now();//advogato, robots
+//			Index index = new Index(IndexUtil.readGraph(Paths.get("C:\\Users\\roanh\\Documents\\2 Thesis\\Datasets\\robots.edge")), 2, false, true, 7, Integer.MAX_VALUE, ProgressListener.LOG);//cores, labels
+//			index.write(Files.newOutputStream(Paths.get("test.bin")), false);
+//			System.out.println("done: " + Duration.between(start, Instant.now()).toString());
+//		}catch(Exception e){
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	/**
+	 * Handles the input arguments.
+	 * @param cli The command line arguments.
+	 */
+	private static void handleInput(CommandLine cli){
+		Path data = Paths.get(cli.getOptionValue('d'));
+		int k = Integer.parseInt(cli.getOptionValue('k'));
+		boolean cores = cli.hasOption('c');
+		boolean labels = cli.hasOption('l');
+		int threads = Integer.parseInt(cli.getOptionValue('t', "1"));
+		int intersections = Integer.parseInt(cli.getOptionValue('i', String.valueOf(Integer.MAX_VALUE)));
+		boolean verbose = cli.hasOption('v');
+		String logFile = verbose ? cli.getOptionValue('v', null) : null;
+		Path output = Paths.get(cli.getOptionValue('o'));
+		boolean full = cli.hasOption('f');
+		
+		try(InputStream in = Files.newInputStream(data)){
+			Index index;
+			if(data.getFileName().endsWith(".idx")){
+				index = new Index(in);
+				if(cores){
+					index.computeCores(threads);
+				}
+			}else{
+				UniqueGraph<Integer, Predicate> g = IndexUtil.readGraph(in);
+				index = new Index(g, k, cores, labels, threads, intersections, verbose ? ProgressListener.stream(new PrintStream(logFile)) : ProgressListener.NONE);
+			}
+
+			index.write(Files.newOutputStream(output), full);
+		}catch(IllegalArgumentException | InterruptedException | ExecutionException | IOException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -73,5 +138,19 @@ public class Main{
 				}
 			}
 		}
+	}
+	
+	static{
+		options = new Options();
+		options.addOption("h", "help", false, "Prints this help text");
+		options.addOption(Option.builder("d").required().longOpt("data").hasArg().argName("file").desc("The graph file to create an index for or a saved index file.").build());
+		options.addOption(Option.builder("k").required().longOpt("diameter").hasArg().argName("k").desc("The value of k (diameter) to compute the index for.").build());
+		options.addOption(Option.builder("c").longOpt("cores").desc("If passed then cores will be computed.").build());
+		options.addOption(Option.builder("l").longOpt("labels").desc("If passed then labels will be computed.").build());
+		options.addOption(Option.builder("t").longOpt("threads").hasArg().argName("number").desc("The number of threads to use for core computation (1 by default).").build());
+		options.addOption(Option.builder("i").longOpt("intersections").hasArg().argName("max").desc("The maximum number of branches for intersection cores (unlimited by default).").build());
+		options.addOption(Option.builder("v").longOpt("verbose").hasArg().optionalArg(true).argName("file").desc("Turns on verbose logging of construction steps, optionally to a file.").build());
+		options.addOption(Option.builder("o").required().longOpt("output").hasArg().argName("file").desc("The file to save the constructed index to.").build());
+		options.addOption(Option.builder("f").longOpt("full").desc("If passed the saved index has all information required to compute cores later.").build());
 	}
 }
--- CPQ-native Index/src/dev/roanh/cpqindex/ProgressListener.java
+++ CPQ-native Index/src/dev/roanh/cpqindex/ProgressListener.java
@@ -1,6 +1,9 @@
 package dev.roanh.cpqindex;
 
+import java.io.PrintStream;
+
 public abstract interface ProgressListener{
+	public static final ProgressListener LOG = stream(System.out);
 	public static final ProgressListener NONE = new ProgressListener(){
 		
 		@Override
@@ -48,64 +51,6 @@ public abstract interface ProgressListener{
 		}
 	};
 	
-	public static final ProgressListener LOG = new ProgressListener(){
-		
-		@Override
-		public void partitionStart(int k){
-			System.out.println(System.currentTimeMillis() + " Partition start k=" + k);
-		}
-		
-		@Override
-		public void partitionEnd(int k){
-			System.out.println(System.currentTimeMillis() + " Partition end k=" + k);
-		}
-		
-		@Override
-		public void partitionCombinationStart(int k1, int k2){
-			System.out.println(System.currentTimeMillis() + " Partition combination start " + k1 + "x" + k2);
-		}
-		
-		@Override
-		public void partitionCombinationEnd(int k1, int k2){
-			System.out.println(System.currentTimeMillis() + " Partition combination end " + k1 + "x" + k2);
-		}
-		
-		@Override
-		public void computeBlocksStart(int k){
-			System.out.println(System.currentTimeMillis() + " Block start k=" + k);
-		}
-		
-		@Override
-		public void computeBlocksEnd(int k){
-			System.out.println(System.currentTimeMillis() + " Block end k=" + k);
-		}
-
-		@Override
-		public void coresStart(int k){
-			System.out.println(System.currentTimeMillis() + " Cores start k=" + k);
-		}
-		
-		@Override
-		public void coresBlocksDone(int done, int total){
-			System.out.println(System.currentTimeMillis() + " Blocks " + done + "/" + total);
-		}
-
-		@Override
-		public void coresEnd(int k){
-			System.out.println(System.currentTimeMillis() + " Cores end k=" + k);
-		}
-
-		@Override
-		public void mapStart(){
-			System.out.println(System.currentTimeMillis() + " Map start");
-		}
-
-		@Override
-		public void mapEnd(){
-			System.out.println(System.currentTimeMillis() + " Map end");
-		}
-	};
-
 	public abstract void partitionStart(int k);
 	
 	public abstract void partitionCombinationStart(int k1, int k2);
@@ -127,4 +72,64 @@ public abstract interface ProgressListener{
 	public abstract void mapStart();
 	
 	public abstract void mapEnd();
+	
+	public static ProgressListener stream(PrintStream out){
+		return new ProgressListener(){
+			
+			@Override
+			public void partitionStart(int k){
+				out.println(System.currentTimeMillis() + " Partition start k=" + k);
+			}
+			
+			@Override
+			public void partitionEnd(int k){
+				out.println(System.currentTimeMillis() + " Partition end k=" + k);
+			}
+			
+			@Override
+			public void partitionCombinationStart(int k1, int k2){
+				out.println(System.currentTimeMillis() + " Partition combination start " + k1 + "x" + k2);
+			}
+			
+			@Override
+			public void partitionCombinationEnd(int k1, int k2){
+				out.println(System.currentTimeMillis() + " Partition combination end " + k1 + "x" + k2);
+			}
+			
+			@Override
+			public void computeBlocksStart(int k){
+				out.println(System.currentTimeMillis() + " Block start k=" + k);
+			}
+			
+			@Override
+			public void computeBlocksEnd(int k){
+				out.println(System.currentTimeMillis() + " Block end k=" + k);
+			}
+
+			@Override
+			public void coresStart(int k){
+				out.println(System.currentTimeMillis() + " Cores start k=" + k);
+			}
+			
+			@Override
+			public void coresBlocksDone(int done, int total){
+				out.println(System.currentTimeMillis() + " Blocks " + done + "/" + total);
+			}
+
+			@Override
+			public void coresEnd(int k){
+				out.println(System.currentTimeMillis() + " Cores end k=" + k);
+			}
+
+			@Override
+			public void mapStart(){
+				out.println(System.currentTimeMillis() + " Map start");
+			}
+
+			@Override
+			public void mapEnd(){
+				out.println(System.currentTimeMillis() + " Map end");
+			}
+		};
+	}
 }
